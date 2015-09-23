610458ebf3c7220566a18dcfff07ed1ab6755b2f
glashunti <gleibsonglas2@yahoo.com.br>
2015-09-23 16:21:52 -0300

    Function
    Function with parameters

--- MinimumValue.cpp
+++ MinimumValue.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
 
+void values(int first, int second);
+
+void display(int first, int second);
+
+
 int main()
 {
 	int first;
 	int second;
+	
+	values(first, second);
+	display(first, second);
 
+	return 0;
+}
+
+void values(int first, int second)
+{
+    
+
+}
+
+void display(int first, int second)
+{
 	std::cout << "Enter the first number: \n";
 	std::cin >> first;
 	std::cout << "Enter the second number: \n";
 	std::cin >> second;
 
 	if (first > second)
-    	std::cout << first << " greater than " << second << "\n";
+		std::cout << first << " greater than " << second << "\n";
 	else
 		std::cout << second << " greater than " << first << "\n";
-	return 0;
+
 }
